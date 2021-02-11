9bc8771db7b82f6936310e58d33fe0a14ce34a7f
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-02-11 11:12:23 -0800

    changed url

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,7 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	var url = $.get('http://localhost:3000/project/' + idNumber, callBackFn);
+	var url = $.get('https://lab6-jc.herokuapp.com/project/' + idNumber, callBackFn);
 
 	console.log("User clicked on project " + idNumber);
 }
